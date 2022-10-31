5121380e45687d83dd72951e5c2ce56af9748c2e
Moiseev Nikolay <niko-light@bk.ru>
2022-10-31 21:01:29 +0300

    На  проверку после исправления - Где сравнение бинов по ссылке, котрое требуется по условию задачи?

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,19 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-        HelloWorld bean2  =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean2.getMessage());
+        HelloWorld bean =(HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2  =(HelloWorld) applicationContext.getBean("helloworld");
 
-        Cat cat1 =
-                (Cat) applicationContext.getBean("cat");
-        System.out.println(cat1.getName());
-        Cat cat2 =
-                (Cat) applicationContext.getBean("cat");
-        System.out.println(cat2.getName());
+        Cat cat1 =(Cat) applicationContext.getBean("cat");
+        Cat cat2 =(Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение бинов \"HelloWorld\"-"+(bean==bean2));
+        System.out.println("Сравнение бинов \"Cat\"-"+(cat1==cat2));
+
+
 
     }
 }
